e0022ba4a5f222abe7764905c293116eb37a90a8
ViniciusSchvepper <vynycyus_hecate@hotmail.com>
2025-04-09 18:03:48 -0300

    chore: change eslint

---
+++ eslint.config.cjs
@@ -0,0 +1,35 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-console': 'error',
+      'no-unused-vars': 'error',
+      semi: ['error', 'never'],
+      'object-curly-spacing': ['error', 'always'],
+      indent: ['error', 'tab'],
+    },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
+];

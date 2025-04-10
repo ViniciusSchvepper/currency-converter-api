9b551f4545c1af7d57e8adb34cdb88a5919e408d
ViniciusSchvepper <vynycyus_hecate@hotmail.com>
2025-04-10 19:23:22 -0300

    chore: prod changes

--- config/config.js
+++ config/config.js
@@ -2,27 +2,17 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_NAME || 'database_development',
-    host: process.env.DB_HOST || '127.0.0.1',
+    use_env_variable: 'DATABASE_URL',
     dialect: 'mysql',
-    port: process.env.DB_PORT || 3306
-  },
-  test: {
-    username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_NAME || 'database_test',
-    host: process.env.DB_HOST || '127.0.0.1',
-    dialect: 'mysql',
-    port: process.env.DB_PORT || 3306
+    dialectOptions: {
+      ssl: false
+    }
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'mysql',
-    port: process.env.DB_PORT || 3306
+    dialectOptions: {
+      ssl: false
+    }
   }
 }
